8309fd0718b82b4eac3ceda2b4a0e85289616dac
Eric Johnson <infinitefoamies@aol.com>
2017-04-19 13:13:55 -0400

    Update Bearing_Hold.ino

--- Bearing_Hold.ino
+++ Bearing_Hold.ino
@@ -18,17 +18,6 @@ void setup() //This code is executed once
   mySensor.setOperationMode(OPERATION_MODE_COMPASS);   //Can be configured to other operation modes as desired
   mySensor.setPowerMode (POWER_MODE_LOWPOWER);          //set powermode to lowpower
   cardinal=mySensor.readMagX();
-  //const byte rows = 4; //four rows
-//const byte cols = 4; //four columns
-//char keys[rows][cols] = {
-  //{'1','2','3','A'},
-  //{'4','5','6','B'},
-  //{'7','8','9','C'},
-  //{'#','0','*','D'}
-//};
-//byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-//byte colPins[cols] = {8, 7, 6}; //connect to the column pinouts of the keypad
-//Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 }
 
 void loop() //This code is looped forever
